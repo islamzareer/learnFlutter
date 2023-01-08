bcc046c27ba030eb104241d1c01b5ef6ea179935
islam zareer <187062@ppu.edu.ps>
2023-01-08 12:08:08 +0200

    login screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  var passwordController = TextEditingController();
+  var emailController = TextEditingController();
+
+  LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                      labelText: "Email Address",
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder()),
+                  controller: emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: "Password",
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.remove_red_eye_sharp),
+                      border: OutlineInputBorder()),
+                  controller: passwordController,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print(emailController.text);
+                      print(passwordController.text);
+                    },
+                    child: const Text(
+                      "LOGIN",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have account yet?"),
+                    TextButton(
+                        onPressed: (() {}), child: const Text("Register now"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
